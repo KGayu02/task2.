361a48c43e7bb198aab2e34484a1502947e4cd63
KGayu02 <109158680+KGayu02@users.noreply.github.com>
2022-07-13 20:52:31 +0530

    Add files via upload

---
+++ task2.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+arr1 = np.array([6, 10, 23, 39, 46, 50])
+print(arr1)
+
+#Indexing
+a1 = arr1.shape
+print(a1)
+
+#Slicing
+print(arr1[1:5])
+
+#Iterating
+arr2 = np.array([10, 20, 30])
+for x in arr2:
+  print(x)
